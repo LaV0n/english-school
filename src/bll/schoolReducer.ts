4dd7c7975465070bede636c0eeed17cf84726fe6
LaV0n <leonidmedica@gmail.com>
2022-11-10 15:45:16 +0100

    feat: add courses

--- src/bll/schoolReducer.ts
+++ src/bll/schoolReducer.ts
@@ -4,11 +4,19 @@ export type UserType={
     email:string
     telephone:string
 }
+export type CoursesType={
+    name:string
+    description:string
+    price:number
+    background:string
+
+}
 type InitialStateType={
     isAdmin:boolean
     password:string
     errorMessage:string | null
     user:UserType
+    courses:CoursesType[]
 }
 
 const initialState:InitialStateType={
@@ -18,7 +26,29 @@ const initialState:InitialStateType={
     user:{
         email:'',
         telephone: ''
-    }
+    },
+    courses:[
+        {name:'Online 7 days per week',
+        description:'Writing is easily done, too – you can send your teacher mini-essays or homework assignments of any sort through email or online learning platforms.',
+        price:400,
+        background:'https://oxfordhousebcn.com/wp-content/uploads/6-Benefits-of-Studying-English-Online.jpg'
+        },
+        {name:'Online 3 days per week',
+            description:'Writing is easily done, too – you can send your teacher mini-essays or homework assignments of any sort through email or online learning platforms.',
+            price:300,
+            background:'https://www.gettingsmart.com/wp-content/uploads/2017/01/Girl-with-Headphones-Using-Laptop-Listening-to-Podcast-Feature-Image.jpg'
+        },
+        {name:'Online 2 days per week',
+            description:'Writing is easily done, too – you can send your teacher mini-essays or homework assignments of any sort through email or online learning platforms.',
+            price:200,
+            background:'https://campustechnology.com/-/media/EDU/CampusTechnology/2018-Images/20180618onlinelearning.jpg'
+        },
+        {name:'Online 1 days per week',
+            description:'Writing is easily done, too – you can send your teacher mini-essays or homework assignments of any sort through email or online learning platforms.',
+            price:100,
+            background:'https://i.cbc.ca/1.5825640.1606940619!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/shutterstock-huge-file.jpg'
+        },
+    ],
 }
 
 const slice=createSlice({
--- src/features/Courses/Courses.tsx
+++ src/features/Courses/Courses.tsx
@@ -1,9 +1,29 @@
 import styles from './Courses.module.scss'
+import {useAppSelector} from "../../bll/store";
 
 export const Courses = ()=>{
+
+    const courser=useAppSelector(state => state.school.courses)
+
     return (
         <div className={styles.container}>
-            Courser
+            <h2>
+                What we have
+            </h2>
+            <div className={styles.block}>
+                {courser.map(c=>
+                <div className={styles.course} style={{backgroundImage:`url(${c.background})`}} key={c.name}>
+                    <div className={styles.cover}>
+                        <h3>
+                            {c.name}
+                        </h3>
+                        <p>
+                            {c.description}
+                        </p>
+                    </div>
+                </div>
+                )}
+            </div>
         </div>
     )
 }
