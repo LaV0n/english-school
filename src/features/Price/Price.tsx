2dd09eaea7f63b45bed094e82c0a981378bc23bd
LaV0n <leonidmedica@gmail.com>
2022-11-11 09:10:30 +0100

    feat: add prices

--- src/features/Price/Price.tsx
+++ src/features/Price/Price.tsx
@@ -1,9 +1,35 @@
 import styles from './Price.module.scss'
+import {useAppSelector} from "../../bll/store";
 
 export const Price = ()=>{
+
+    const courses=useAppSelector(state => state.school.courses)
+
     return (
         <div className={styles.container}>
-            Price
+            <h2>
+                Our courses
+            </h2>
+            <h4>
+                Choose that you want
+            </h4>
+            <div className={styles.block}>
+                {courses.map(c=>
+                <div className={styles.course} key={c.name}>
+                    <h3>
+                        {c.name}
+                    </h3>
+                    <p>
+                        {c.description}
+                    </p>
+                    <p> Price from
+                        <span>
+                            {c.price}
+                        </span>
+                     per mouth</p>
+                </div>
+                )}
+            </div>
         </div>
     )
 }
